a4e6f26c779571ec7804f3bae715f9dd713bf62f
ganesh sonar <ganeshs18@birlasoft.com>
2021-10-03 01:25:23 +0530

    Home Doctorlist intial

---
+++ src/screens/appointment/Appointment.js
@@ -0,0 +1,18 @@
+import {React, Typography} from "../../component";
+
+
+function Appointment(props){
+
+
+    return( <React.Fragment>
+
+
+        <Typography>
+
+            Appointment
+        </Typography>
+
+    </React.Fragment>)
+
+}
+export default Appointment
---
+++ src/screens/doctorList/DoctorList.js
@@ -0,0 +1,15 @@
+import {React, Typography} from "../../component";
+
+
+function DoctorList(props){
+
+
+    return( <React.Fragment>
+
+<Typography>
+    Doctor list
+</Typography>
+    </React.Fragment>)
+
+}
+export default DoctorList
